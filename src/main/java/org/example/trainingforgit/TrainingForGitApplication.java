552223cb4f7f540618a7ecf1e00870367737147c
Aliev Vagif <aliev.vagif69@gmail.com>
2025-01-12 04:36:48 +0300

    first commit

---
+++ src/main/java/org/example/trainingforgit/TrainingForGitApplication.java
@@ -0,0 +1,13 @@
+package org.example.trainingforgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingForGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingForGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/trainingforgit/TrainingForGitApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trainingforgit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingForGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
